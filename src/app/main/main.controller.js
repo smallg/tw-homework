ad0096b4d371070087962a0c316113e04209d740
smallg <gwpxjb@163.com>
2016-07-15 21:25:33 +0800

    add tab content

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,6 +9,43 @@
     function MainController($timeout, webDevTec, toastr) {
         var vm = this;
 
+        vm.activeTabName = 'agents';
+        vm.setActiveTab = function (e) {
+            if (angular.isDefined(e)) {
+                vm.activeTabName = e.currentTarget.innerHTML;
+            }
 
+        };
+
+        vm.result = [
+            {
+                address: "bjstdmngbgr02.thoughtworks.com",
+                type: "idle",
+                ipAddress: "192.168.1.3",
+                localAddress: "/var/lib/cruise-agent",
+                resources: [{name: "ubuntu"}, {name: "firefox3"}, {name: "core-duo"}]
+            },
+            {
+                address: "bjstdmngbgr03.thoughtworks.com",
+                type: "building",
+                ipAddress: "192.168.1.3",
+                localAddress: "/var/lib/cruise-agent",
+                resources: [{name: "ubuntu"}, {name: "firefox3"}, {name: "mysql"}, {name: "core-duo"}]
+            },
+            {
+                address: "bjstdmngbgr04.thoughtworks.com",
+                type: "building",
+                ipAddress: "192.168.1.4",
+                localAddress: "/var/lib/cruise-agent",
+                resources: [{name: "ubuntu"}, {name: "firefox3"}, {name: "core-duo"}]
+            },
+            {
+                address: "bjstdmngbgr05.thoughtworks.com",
+                type: "idle",
+                ipAddress: "192.168.1.5",
+                localAddress: "/var/lib/cruise-agent",
+                resources: [{name: "ubuntu"}]
+            }
+        ];
     }
 })();
